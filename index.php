c9c021eeca1f12974e700dc52b0068c46ca53006
Glenowenn <129427898+Glenowenn@users.noreply.github.com>
2023-10-22 22:16:43 +0700

    to do list owen

--- index.php
+++ index.php
@@ -1,86 +1,109 @@
-<!DOCTYPE HTML>
-<html>
+<?php 
+require 'db_conn.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<title>Simple To-Do List</title>
-	<link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>To-Do List</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<div class="wrap">
-		<div class="task-list">
-			<ul>
+    <div class="main-section">
+       <div class="add-section">
+          <form action="app/add.php" method="POST" autocomplete="off">
+             <?php if(isset($_GET['mess']) && $_GET['mess'] == 'error'){ ?>
+                <input type="text" 
+                     name="title" 
+                     style="border-color: #ff6666"
+                     placeholder="This field is required" />
+              <button type="submit">Add &nbsp; <span>&#43;</span></button>
 
-			<?php 
-				require("includes/connect.php");
+             <?php }else{ ?>
+              <input type="text" 
+                     name="title" 
+                     placeholder="What do you need to do?" />
+              <button type="submit">Add &nbsp; <span>&#43;</span></button>
+             <?php } ?>
+          </form>
+       </div>
+       <?php 
+          $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC");
+       ?>
+       <div class="show-todo-section">
+            <?php if($todos->rowCount() <= 0){ ?>
+                <div class="todo-item">
+                    <div class="empty">
+                        <img src="img/f.png" width="100%" />
+                        <img src="img/Ellipsis.gif" width="80px">
+                    </div>
+                </div>
+            <?php } ?>
 
-				$query = $conn->query("SELECT * FROM tasks ORDER BY date ASC, time ASC");
-				$numrows = $query->num_rows;
+            <?php while($todo = $todos->fetch(PDO::FETCH_ASSOC)) { ?>
+                <div class="todo-item">
+                    <span id="<?php echo $todo['id']; ?>"
+                          class="remove-to-do">x</span>
+                    <?php if($todo['checked']){ ?> 
+                        <input type="checkbox"
+                               class="check-box"
+                               data-todo-id ="<?php echo $todo['id']; ?>"
+                               checked />
+                        <h2 class="checked"><?php echo $todo['title'] ?></h2>
+                    <?php }else { ?>
+                        <input type="checkbox"
+                               data-todo-id ="<?php echo $todo['id']; ?>"
+                               class="check-box" />
+                        <h2><?php echo $todo['title'] ?></h2>
+                    <?php } ?>
+                    <br>
+                    <small>created: <?php echo $todo['date_time'] ?></small> 
+                </div>
+            <?php } ?>
+       </div>
+    </div>
 
-				if($numrows>0){
-						while($row = $query->fetch_assoc()) {
+    <script src="js/jquery-3.2.1.min.js"></script>
 
-						$task_id = $row['id'];
-						$task_name = $row['task'];
+    <script>
+        $(document).ready(function(){
+            $('.remove-to-do').click(function(){
+                const id = $(this).attr('id');
+                
+                $.post("app/remove.php", 
+                      {
+                          id: id
+                      },
+                      (data)  => {
+                         if(data){
+                             $(this).parent().hide(600);
+                         }
+                      }
+                );
+            });
 
-						echo '<li>
-								<span>'.$task_name.'</span>
-								<img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg" />
-							</li>';
-					}
-				}
-
-			?>
-				
-			</ul>
-		</div>
-		<form class="add-new-task" autocomplete="off">
-			<input type="text" name="new-task" placeholder="Add a new item..." />
-		</form>
-	</div><!-- #wrap -->
+            $(".check-box").click(function(e){
+                const id = $(this).attr('data-todo-id');
+                
+                $.post('app/check.php', 
+                      {
+                          id: id
+                      },
+                      (data) => {
+                          if(data != 'error'){
+                              const h2 = $(this).next();
+                              if(data === '1'){
+                                  h2.removeClass('checked');
+                              }else {
+                                  h2.addClass('checked');
+                              }
+                          }
+                      }
+                );
+            });
+        });
+    </script>
 </body>
-	<!-- JavaScript Files Go Here -->
-	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
-	<script>
-		
-		add_task(); // Call the add_task function
-		delete_task(); // Call the delete_task function
-
-		function add_task() {
-
-			$('.add-new-task').submit(function(){
-
-				var new_task = $('.add-new-task input[name=new-task]').val();
-
-				if(new_task != ''){
-
-					$.post('includes/add-task.php', { task: new_task }, function( data ) {
-
-						$('.add-new-task input[name=new-task]').val('');
-
-						$(data).appendTo('.task-list ul').hide().fadeIn();
-
-						delete_task();
-					});
-				}
-
-				return false; // Ensure that the form does not submit twice
-			});
-		}
-
-		function delete_task() {
-
-			$('.delete-button').click(function(){
-
-				var current_element = $(this);
-
-				var id = $(this).attr('id');
-
-				$.post('includes/delete-task.php', { task_id: id }, function() {
-
-					current_element.parent().fadeOut("fast", function() { $(this).remove(); });
-				});
-			});
-		}
-
-	</script>
-
 </html>
